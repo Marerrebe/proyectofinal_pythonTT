7ce61ef429b0d72d02c4f161b0d606fbdee69094
Marcia <mruizbochides@gmail.com>
2026-06-13 21:25:33 -0300

    funcion menu

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 print ("Hola")
 
-def menu():
+def menu() -> None:
+    """
+    menu de tienda 
+
+    """
+
+    print ("""
+    1- crear producto
+    2- ver productos
+    3- buscar productos
+    4- eliminar productos
+    5- salir
+    """
+        )
     
+    menu ()
+   
